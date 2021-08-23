f463d108b010ec1a1163de63be9b3d4eeca1f906
clistenes-fernandes <clisteness.fernandes@gmail.com>
2021-08-23 01:02:56 +0100

    Hr-payroll Ribbon implementation

--- hr/hr-payroll/src/main/java/com/cfs/hrpayroll/resources/PaymentResource.java
+++ hr/hr-payroll/src/main/java/com/cfs/hrpayroll/resources/PaymentResource.java
@@ -1,7 +1,10 @@
 package com.cfs.hrpayroll.resources;
 
 import com.cfs.hrpayroll.entities.Payment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,7 @@ import com.cfs.hrpayroll.services.PaymentService;
 @RestController
 @RequestMapping(value = "payments")
 public class PaymentResource {
+
 
     @Autowired
     private PaymentService paymentService;
